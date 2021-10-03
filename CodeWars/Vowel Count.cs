856f74f4943ccf92bd6cfe2b6514f82b5642d74f
kashdevine <kasheen.mowatt@gmail.com>
2021-10-03 12:05:05 -0700

    Add project files.

---
+++ CodeWars/Vowel Count.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class Vowel_Count
+    {
+        public int GetVowelCount(string str)
+        {
+            int vowelCount = 0;
+            char[] vowels = {'a','e','i','o','u' };
+            foreach (char c in str)
+            {
+                if (vowels.Contains(c)){
+                    vowelCount++;
+                }
+            }
+
+            return vowelCount;
+        }
+    }
+}
