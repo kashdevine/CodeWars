597c0dcf39aa16a9a0d95abe03e8631d07ae6d1b
kashdevine <kasheen.mowatt@gmail.com>
2021-10-03 15:19:29 -0700

    Detect Pangram solution and unit tests created.

---
+++ CodeWars/DetetctPangram.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class DetetctPangram
+    {
+        public static bool IsPangram(string str)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            char[] charsToTrim = {'.', '!', '?', '(',')', ',', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            
+            
+
+           char[] justLetters =  str.ToLower()
+                .Trim(charsToTrim)
+                .Replace(" ", "")
+                .Distinct()
+                .ToArray();
+
+            Array.Sort(justLetters);
+
+
+
+            string sortedInputString = new string(justLetters);
+
+            if (sortedInputString.Length != 26)
+            {
+                foreach (char c in sortedInputString)
+                {
+                    if (charsToTrim.Contains(c))
+                    {
+                        int removeIndex = sortedInputString.IndexOf(c);
+                        sortedInputString = sortedInputString.Remove(removeIndex,1);
+                    }
+                }
+            }
+
+
+            if (alphabet == sortedInputString)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ SanityChecks/DetetctPangramTest.cs
@@ -0,0 +1,40 @@
+﻿using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace SanityChecks
+{
+    public static class DetetctPangramTest
+    {
+        
+        [Theory]
+        [InlineData("The quick brown fox jumps over the lazy dog.")]
+        [InlineData("ABCD45EFGH,IJK,LMNOPQR56STUVW3XYZ")]
+        public static void IsPangram_ReturnsTrue_IfStringIsPangram(string s)
+        {
+            //arrange
+            string test = s;
+            bool expected = true;
+            //act
+            bool result = DetetctPangram.IsPangram(test);
+            //assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public static void IsPangram_ReturnsFalse_IfStringIsNotPangram()
+        {
+            //arrange
+            string test = "This is hollywood.";
+            bool expected = false;
+            //act
+            bool result = DetetctPangram.IsPangram(test);
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
