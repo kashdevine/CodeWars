86f9f73de1a494f08f8a0692a227262d8adc7b55
kashdevine <kasheen.mowatt@gmail.com>
2021-10-03 19:42:37 -0700

    Created solution to Exs and Ohs question.

---
+++ CodeWars/ExesandOhs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class ExesandOhs
+    {
+        public static bool XO(string input)
+        {
+            
+            int Xs = input.ToLower().Count(x => x == 'x');
+            int Ohs = input.ToLower().Count(o => o == 'o');
+
+            return Xs == Ohs;
+        }
+    }
+}
---
+++ SanityChecks/ExesandOhsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CodeWars;
+
+namespace SanityChecks
+{
+    public static class ExesandOhsTest
+    {
+        [Theory]
+        [InlineData("ooxx", true)]
+        [InlineData("xooxx", false)]
+        [InlineData("ooxXm", true)]
+        [InlineData("zpzpzpp", true)]
+        [InlineData("zzoo", false)]
+        public static void XO_Returns_CorrectBoolForCounts(string input, bool e)
+        {
+            //arrange
+            string sut = input;
+            bool expected = e;
+
+            //act
+            bool result = ExesandOhs.XO(sut);
+
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
