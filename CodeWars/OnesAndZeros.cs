692a30b2343e71bbf80436361ce7f891cec7c27d
kashdevine <kasheen.mowatt@gmail.com>
2021-10-03 22:04:24 -0700

    Created solution to Ones and Zeros  keta.

---
+++ CodeWars/OnesAndZeros.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class OnesAndZeros
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            string binary = string.Join(string.Empty, BinaryArray);
+            return Convert.ToInt32(binary, 2);
+        }
+    }
+}
---
+++ SanityChecks/OnesAndZerosTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CodeWars;
+
+namespace SanityChecks
+{
+    public static class OnesAndZerosTest
+    {
+        [Theory]
+        [InlineData(new int[] { 0, 0, 0, 0 }, 0)]
+        [InlineData(new int[] { 1, 1, 1, 1 }, 15)]
+        public static void binaryArrayToNumber_Returns_CorrectIntfromBinary(int[] input, int output)
+        {
+            //arrange
+            int[] sut = input;
+            int expected = output;
+            //act
+            int result = OnesAndZeros.binaryArrayToNumber(sut);
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
