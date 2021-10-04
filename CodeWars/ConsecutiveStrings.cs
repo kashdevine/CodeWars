6717e49c2178f3a9f56eaba163399c686fd9d449
kashdevine <kasheen.mowatt@gmail.com>
2021-10-03 21:49:52 -0700

    Attempted solution at consecutive strings kata.

---
+++ CodeWars/ConsecutiveStrings.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public static class ConsecutiveStrings
+    {
+        public static string LongestConcet(string[] strarr, int k)
+        {
+            int n = strarr.Length;
+            List<string> longStrings = new List<string>();
+            int originalK = k;
+
+            if(n == 0 || k > n || k <= 0)
+            {
+                return "";
+            }
+
+            if (k == 1)
+            {
+                for(int i=0; i<n; i++)
+                {
+                    string temp = strarr[i];
+
+                    if (longStrings.Count == 0)
+                    {
+                        longStrings.Add(temp);
+                    }
+
+                    else if (temp.Length > longStrings[0].Length)
+                    {
+                        longStrings.Insert(0, temp);
+                    }
+
+                    else if (temp.Length == longStrings[0].Length)
+                    {
+                        continue;
+                    }
+                }
+
+                return longStrings[0];
+
+            }
+
+            for (int i=0; i < n; i++)
+            {
+                string temp = string.Empty;
+
+
+
+                int j = i + 1;
+                int m = j + 1;
+
+
+                if (j >= n)
+                {
+                    return longStrings[0];
+                }
+
+                temp = strarr[i] + strarr[j];
+
+                while(k>2)
+                {
+                    if (m >= n)
+                    {
+                        return longStrings[0];
+                    }
+                    temp = temp + strarr[m];
+                    m++;
+                    k--;
+                }
+
+                if (longStrings.Count == 0)
+                {
+                    longStrings.Add(temp);
+                }
+
+                else if(temp.Length > longStrings[0].Length)
+                {
+                    longStrings.Insert(0, temp);
+                }
+                
+                else if(temp.Length == longStrings[0].Length)
+                {
+                    continue;
+                }
+
+                k = originalK;
+            }
+
+
+            return longStrings[0];
+        }
+    }
+}
---
+++ SanityChecks/ConsecutiveStringsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using CodeWars;
+
+namespace SanityChecks
+{
+    public static class ConsecutiveStringsTest
+    {
+        [Theory]
+        [InlineData(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2,"abigailtheta" )]
+        [InlineData(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1, "oocccffuucccjjjkkkjyyyeehh")]
+        [InlineData(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2, "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")]
+        [InlineData(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3, "ixoyx3452zzzzzzzzzzzz")]
+        public static void LongestConcet_Returns_LongConcatOfStrings(string[] strarr, int k, string output)
+        {
+            //arrange
+            string[] input = strarr;
+            string expected = output;
+            //act
+            string result = ConsecutiveStrings.LongestConcet(input, k);
+
+            //assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
